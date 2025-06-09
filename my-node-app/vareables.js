dcc846cf6a2b459fc787d0582c8b4b0bf0cbfba5
princeyadav147147 <princeyadavpy4697588@gmail.com>
2025-06-09 17:25:43 +0530

    adding new file of javascript

---
+++ my-node-app/vareables.js
@@ -0,0 +1,12 @@
+ const accountId = '8826'
+ let accountName = 'adil'
+ let accountEmail = 'prince@gmail.com'
+ let accountPassword = '147147'
+ let accountCity = 'delhi'
+
+accountCity = 'mumbai'
+accountPassword = '882641'
+
+
+console.table([accountId, accountName, accountEmail, accountPassword, accountCity]);
+
