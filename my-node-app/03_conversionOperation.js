0d0a5333f4d8317cc6612416c104afeca383f87f
princeyadav147147 <princeyadavpy4697588@gmail.com>
2025-06-10 23:56:48 +0530

    adding new file

---
+++ my-node-app/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "Null"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+ 
+let vlyeInNumber =Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1; false =>0   
+
+let isLoggedIn = "prince"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; o => false
+// "" => false
+// "prince" => true
+
+let some Number =33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
