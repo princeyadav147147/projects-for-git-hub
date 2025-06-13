f2d0c56e66512838e2d103ff786bc562cee0e670
princeyadav147147 <princeyadavpy4697588@gmail.com>
2025-06-13 16:55:32 +0530

    changis

--- my-node-app/04_comparision.js
+++ my-node-app/04_comparision.js
@@ -7,14 +7,14 @@
 //console.log("2" > 1);
 //console.log("02" > 1);
 
-//console.log(null > 0);
-//console.log(null == 0);
-//console.log(null >= 0);
+//console.log(null > 0); //false
+//console.log(null == 0); //false
+//console.log(null >= 0); //true
 
-//console.log(undefined == 0);
-//console.log(undefined > 0);
-//console.log(undefined < 0);
+//console.log(undefined == 0); // flase
+//console.log(undefined > 0); // flase
+//console.log(undefined < 0); // flase
 
 // ===
 
-console.log("2" === 2);
+console.log("2" === 2); //false
